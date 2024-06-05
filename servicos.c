c335e8de218b1a99d9b6a029486ab242affd0067
Diogo Ataídes <54185298+dioats@users.noreply.github.com>
2024-06-04 21:00:57 -0300

    Update servicos.c

--- servicos.c
+++ servicos.c
@@ -1,1 +1,48 @@
+#include <stdio.h>
 
+
+// Serviço Realizado: 
+//   - consumidor
+//   - prestador do serviço
+//   - 
+
+// Prestador do serviço: 
+//   - Nome
+//   - e-mail
+//   - telefone 
+//   - endereço 
+//   - UF
+
+// Consumidor: 
+//   - Nome 
+//   - idade 
+//   - e-mail
+//   - telefone 
+//   - endereço
+//   - UF
+
+// Serviço: 
+//   - Detalhamento do serviço 
+//   - data do serviço prestado 
+//   - preço cobrado.
+//   - Valor do serviço
+//   - Descrição
+//   - tipo de serviço 
+
+// Criar um menu que possibilite executar as seguintes operações:
+
+// Listar todos os tipos de serviços.
+// Listar todas os clientes.
+// Listar todos os prestadores de serviços.
+// Listar os clientes de um determinado estado.
+// Listar os prestadores serviço de um determinado tipo.
+// Apresentar o(s) estado(s) onde está registrado o serviço mais caro.
+// Listar todos os serviços em ordem crescente de valor.
+// Listar todos os clientes em ordem crescente de nome.
+
+int main()
+{
+    printf("Hello World");
+
+    return 0;
+}
